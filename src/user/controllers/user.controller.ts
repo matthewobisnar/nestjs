c234a19a730df96d0c1e5795131b90039fe3b468
matthew <matt.bisnar@emapta.com>
2024-02-01 22:09:40 +0800

    added matthew projects

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -4,11 +4,20 @@ import { Roles } from 'src/shared/decorators/roles.decorator';
 import { IsAuthenticateGuard } from 'src/shared/guards/is.authenticate.guard';
 import { IsAuthorizedGuard } from 'src/shared/guards/is.authorized.guard';
 import { SwaggerExceptionResponseDto } from 'src/shared/swagger/swagger.exception.response.dto';
+import { UserService } from '../services/user/user.service';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
+
+    constructor(private readonly userService: UserService) {}
     
+    @ApiBearerAuth()
+    @ApiOkResponse()
+    @Roles(["ADMIN", "USER"])
+    @UseGuards(IsAuthenticateGuard, IsAuthorizedGuard)
+    @ApiUnauthorizedResponse({ type: SwaggerExceptionResponseDto })
+    @ApiForbiddenResponse({ type: SwaggerExceptionResponseDto })
     @Get('profile/:id')
     getUserProfile(@Param('id') id: string) {
         return id;
@@ -16,13 +25,13 @@ export class UserController {
 
     @ApiBearerAuth()
     @ApiOkResponse()
-    @Roles(["ADMIN", "USER"])
-    @Get('/role/profile/:id')
+    @Roles(["ADMIN"])
+    @Get('/profile/all')
     @UseGuards(IsAuthenticateGuard, IsAuthorizedGuard)
     @ApiUnauthorizedResponse({ type: SwaggerExceptionResponseDto })
     @ApiForbiddenResponse({ type: SwaggerExceptionResponseDto })
-    getUserRole(@Param('id') id: string) {
-        return "Hello world";
+    getUsers() {
+        return this.userService.getUsers();
     }
 
 }
--- src/user/services/user/user.service.ts
+++ src/user/services/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/shared/db/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    
+    constructor(@InjectRepository(UserEntity) private readonly userEntityRepository: Repository<UserEntity>) {}
+
+    async getUsers() {
+        return await this.userEntityRepository.find({
+            relations: ['roles', 'roles.role'],
+        });
+    }
+}
