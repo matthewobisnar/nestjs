740782e65fd7590fb8ee7580af3858e073c1f537
matthew <matt.bisnar@emapta.com>
2024-02-02 01:32:35 +0800

    clean swagger repository

--- src/shared/swagger/swagger.config.ts
+++ src/shared/swagger/swagger.config.ts
@@ -4,8 +4,6 @@ export const swaggerConfig = new DocumentBuilder()
 .setTitle('Ticketing Swagger API')
 .setDescription('The ticketing API description.')
 .setVersion('1.0')
-.addTag('user')
-.addTag('role')
 .addBearerAuth(
     { 
       description: `[just text field] Please enter token in following format: Bearer <JWT>`,
