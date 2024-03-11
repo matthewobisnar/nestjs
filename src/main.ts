6898d1880a5588715f6e0ff2a22d44189022a74c
matthew <matt.bisnar@emapta.com>
2024-03-11 09:06:35 +0800

    update npm package manager

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './shared/swagger/swagger.config';
-import { swaggerThemeOptions } from './shared/swagger/swagger.theme.config';
+// import { swaggerThemeOptions } from './shared/swagger/swagger.theme.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   SwaggerModule.setup(
     'docs', app, 
-    SwaggerModule.createDocument(app, swaggerConfig),
-    swaggerThemeOptions
+    SwaggerModule.createDocument(app, swaggerConfig)
   );
 
   await app.listen(process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3000);
--- src/shared/swagger/swagger.theme.config.ts
+++ src/shared/swagger/swagger.theme.config.ts
@@ -1,7 +1,7 @@
 import { SwaggerTheme } from "swagger-themes";
 
 const theme = new SwaggerTheme('v3');
-export const swaggerThemeOptions = {
-  explorer: true,
-  customCss: theme.getBuffer('dark')
-};
+// export const swaggerThemeOptions = {
+//   explorer: true,
+//   customCss: theme.getBuffer('dark')
+// };
