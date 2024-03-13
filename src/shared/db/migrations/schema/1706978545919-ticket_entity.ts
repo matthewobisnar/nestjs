4781ab247e919a9190f5bef0c73ef2396aa055e2
matthew bisnar <matthewobisnar@gmail.com>
2024-03-14 00:41:30 +0800

    added migration

--- src/shared/db/migrations/schema/1706978545919-ticket_entity.ts
+++ src/shared/db/migrations/schema/1706978545919-ticket_entity.ts
@@ -4,7 +4,7 @@ export class TicketEntity1706978545919 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(
-            `CREATE TABLE public.ticket_entity (
+            `CREATE TABLE ticket_entity (
                 created_at timestamp NOT NULL,
                 created_by varchar NOT NULL,
                 updated_at timestamp NULL,
@@ -19,8 +19,8 @@ export class TicketEntity1706978545919 implements MigrationInterface {
                 CONSTRAINT "PK_4c23bb38e4d566808a73a5af6ec" PRIMARY KEY (id),
                 CONSTRAINT "REL_872bf60722bdaff3118a1d2342" UNIQUE (status),
                 CONSTRAINT "REL_a60cb0c083243ffb156246ec0e" UNIQUE (assignee),
-                CONSTRAINT "FK_872bf60722bdaff3118a1d2342c" FOREIGN KEY (status) REFERENCES public.ticket_status(id),
-                CONSTRAINT "FK_a60cb0c083243ffb156246ec0e1" FOREIGN KEY (assignee) REFERENCES public.user_entity(id)
+                CONSTRAINT "FK_872bf60722bdaff3118a1d2342c" FOREIGN KEY (status) REFERENCES ticket_status(id),
+                CONSTRAINT "FK_a60cb0c083243ffb156246ec0e1" FOREIGN KEY (assignee) REFERENCES user_entity(id)
             );`
         );
     }
--- src/shared/db/migrations/schema/1706978724345-ticket_status.ts
+++ src/shared/db/migrations/schema/1706978724345-ticket_status.ts
@@ -4,7 +4,7 @@ export class TicketStatus1706978724345 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(`
-            CREATE TABLE public.ticket_status (
+            CREATE TABLE ticket_status (
                 created_at timestamp NOT NULL,
                 created_by varchar NOT NULL,
                 updated_at timestamp NULL,
@@ -17,7 +17,7 @@ export class TicketStatus1706978724345 implements MigrationInterface {
                 "statusTicketnameId" int4 NULL,
                 CONSTRAINT "PK_a39055e902c270197f3711e0ee3" PRIMARY KEY (id),
                 CONSTRAINT "REL_13b9703c422ef13e976ed937e2" UNIQUE ("statusTicketnameId"),
-                CONSTRAINT "FK_13b9703c422ef13e976ed937e2a" FOREIGN KEY ("statusTicketnameId") REFERENCES public.ticket_entity(id)
+                CONSTRAINT "FK_13b9703c422ef13e976ed937e2a" FOREIGN KEY ("statusTicketnameId") REFERENCES ticket_entity(id)
             );
         `);
     }
