01643ac73ae1ebba25e1a858c9646c69ac0a0144
matthew bisnar <matthewobisnar@gmail.com>
2024-03-22 15:53:17 +0800

    added passport auth to user

--- src/core/modules/user/controllers/user.controller.ts
+++ src/core/modules/user/controllers/user.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiForbiddenResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiCreatedResponse, ApiForbiddenResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { Roles } from 'src/shared/decorators/roles.decorator';
 import { SwaggerExceptionResponseDto } from 'src/shared/configs/swagger/swagger.exception.response.dto';
-import { IsAuthenticateGuard } from '../../authentication/guards/is.authenticate.guard';
-import { IsAuthorizedGuard } from '../../authentication/guards/is.authorized.guard';
 import { UserService } from '../services/user/user.service';
+import { SignupUserRequestDto } from 'src/shared/dtos/signup.user.request.dto';
+import { AuthJwtGuard } from '../../passport/strategies/jwt/auth.jwt.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('User Profile')
 @Controller('user')
@@ -12,10 +13,10 @@ export class UserController {
 
     constructor(private readonly userService: UserService) {}
     
-    @ApiBearerAuth()
     @ApiOkResponse()
-    @Roles(["ADMIN", "USER"])
-    @UseGuards(IsAuthenticateGuard, IsAuthorizedGuard)
+    @ApiBearerAuth()
+    @Roles(["USER", "ADMIN"])
+    @UseGuards(AuthGuard('jwt'), AuthJwtGuard)
     @ApiUnauthorizedResponse({ type: SwaggerExceptionResponseDto })
     @ApiForbiddenResponse({ type: SwaggerExceptionResponseDto })
     @Get('profile/:id')
@@ -25,13 +26,21 @@ export class UserController {
 
     @ApiBearerAuth()
     @ApiOkResponse()
-    @Roles(["ADMIN"])
     @Get('/all')
-    @UseGuards(IsAuthenticateGuard, IsAuthorizedGuard)
+    @Roles(["ADMIN"])
+    @UseGuards(AuthGuard('jwt'), AuthJwtGuard)    
     @ApiUnauthorizedResponse({ type: SwaggerExceptionResponseDto })
     @ApiForbiddenResponse({ type: SwaggerExceptionResponseDto })
     getUsers() {
         return this.userService.getUsers();
+    }
+
+    @Post('/signup')
+    @ApiCreatedResponse()
+    @UseGuards(AuthGuard('jwt'), AuthJwtGuard)  
+    @ApiBadRequestResponse({type: SwaggerExceptionResponseDto})
+    signup(@Body() body: SignupUserRequestDto) {
+       return this.userService.registerUser({...body});
     }
 
 }
--- src/core/modules/user/services/user/user.service.ts
+++ src/core/modules/user/services/user/user.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/infrastructure/entities/user.entity';
+import { SignupUserRequestDto } from 'src/shared/dtos/signup.user.request.dto';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { UserRoleEntity } from 'src/infrastructure/entities/user.role.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
 
-    constructor(@InjectRepository(UserEntity) private readonly userEntityRepository: Repository<UserEntity>) {}
+    constructor(
+        @InjectRepository(UserEntity) private readonly userEntityRepository: Repository<UserEntity>,
+        @InjectRepository(UserRoleEntity) private readonly userRoleEntityRepository: Repository<UserRoleEntity>,
+        private readonly jwtService: JwtService
+    ) {}
 
     async getUsers() {
         return await this.userEntityRepository.find({
@@ -19,5 +27,44 @@ export class UserService {
             where: { id: 1},
             relations: ['roles'],
         });
+    }
+
+    private async byecriptEncrypt(password: string) : Promise<string> {
+        return await bcrypt.hash(password, await bcrypt.genSalt());
+    }
+
+    public async registerUser(user: SignupUserRequestDto) {
+
+        const isUserExists = await this.userEntityRepository.countBy({ email: user.email });
+
+        console.log(isUserExists);
+
+        if (isUserExists > 0) {
+            throw new BadRequestException(`${user.email} is already exists.`)
+        }
+
+        const userEntity: UserEntity = this.userEntityRepository.create({
+            ...user,
+            password: await this.byecriptEncrypt(user.password),
+            createdAt: new Date(),
+            createdBy: '00-00000'
+        });
+        
+        const result: UserEntity = await this.userEntityRepository.save(userEntity);
+
+        if (result != null) {
+
+            const userRoleEntity: UserRoleEntity = this.userRoleEntityRepository.create({
+                userId: result.id,
+                roleName: 'USER',
+                createdAt: new Date(),
+                createdBy: result.email
+            });
+
+            await this.userRoleEntityRepository.save(userRoleEntity);
+            return result;
+        }
+        
+        return null;
     }
 }
